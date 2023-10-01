a996fc741ee64c99659eb044f0ca03caaeacb692
bokwoon <bokwoon.c@gmail.com>
2023-10-01 18:12:15 +0800

    checkpoint

--- createpage.go
+++ createpage.go
@@ -2,6 +2,7 @@ package nb7
 
 import (
 	"encoding/json"
+	"fmt"
 	"html/template"
 	"io/fs"
 	"mime"
@@ -89,6 +90,77 @@ func (nbrew *Notebrew) createpage(w http.ResponseWriter, r *http.Request, userna
 		response.Status = Success
 		writeResponse(w, r, response)
 	case "POST":
+		writeResponse := func(w http.ResponseWriter, r *http.Request, response Response) {
+			accept, _, _ := mime.ParseMediaType(r.Header.Get("Accept"))
+			if accept == "application/json" {
+				w.Header().Set("Content-Type", "application/json")
+				encoder := json.NewEncoder(w)
+				encoder.SetEscapeHTML(false)
+				err := encoder.Encode(&response)
+				if err != nil {
+					getLogger(r.Context()).Error(err.Error())
+				}
+				return
+			}
+			var status, redirectURL string
+			if response.Status.Equal(ErrParentFolderNotProvided) || response.Status.Equal(ErrInvalidParentFolder) {
+				status = response.Status.Code() + " Couldn't create item, " + response.Status.Message()
+				redirectURL = nbrew.Scheme + nbrew.AdminDomain + "/" + path.Join("admin", sitePrefix) + "/"
+			} else if response.Status.Equal(ErrForbiddenFolderName) || response.Status.Equal(ErrFolderAlreadyExists) {
+				status = string(response.Status)
+				redirectURL = nbrew.Scheme + nbrew.AdminDomain + "/" + path.Join("admin", sitePrefix, response.ParentFolder) + "/"
+			} else if response.Status.Equal(CreateFolderSuccess) {
+				status = fmt.Sprintf(
+					`%s Created page <a href="%s" class="linktext">%s</a>`,
+					response.Status.Code(),
+					"/"+path.Join("admin", sitePrefix, response.ParentFolder, response.Name)+"/",
+					response.Name,
+				)
+				redirectURL = nbrew.Scheme + nbrew.AdminDomain + "/" + path.Join("admin", sitePrefix, response.ParentFolder) + "/"
+			}
+			err := nbrew.setSession(w, r, "flash", map[string]any{
+				"status": status,
+			})
+			if err != nil {
+				getLogger(r.Context()).Error(err.Error())
+				internalServerError(w, r, err)
+				return
+			}
+			http.Redirect(w, r, redirectURL, http.StatusFound)
+		}
+		_ = writeResponse
+
+		var request Request
+		contentType, _, _ := mime.ParseMediaType(r.Header.Get("Content-Type"))
+		switch contentType {
+		case "application/json":
+			err := json.NewDecoder(r.Body).Decode(&request)
+			if err != nil {
+				badRequest(w, r, err)
+				return
+			}
+		case "application/x-www-form-urlencoded", "multipart/form-data":
+			if contentType == "multipart/form-data" {
+				err := r.ParseMultipartForm(2 << 20 /* 2MB */)
+				if err != nil {
+					badRequest(w, r, err)
+					return
+				}
+			} else {
+				err := r.ParseForm()
+				if err != nil {
+					badRequest(w, r, err)
+					return
+				}
+			}
+			request.ParentFolder = r.Form.Get("parentFolder")
+			request.Name = r.Form.Get("name")
+			request.Content = r.Form.Get("content")
+		default:
+			unsupportedContentType(w, r)
+			return
+		}
 	default:
+		methodNotAllowed(w, r)
 	}
 }
