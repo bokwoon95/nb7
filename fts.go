02728f514871de5f392fdb23c2af375927f37840
bokwoon <bokwoon.c@gmail.com>
2023-10-11 03:13:19 +0800

    checkpoint

--- fts.go
+++ fts.go
@@ -1,10 +1,15 @@
 package nb7
 
-import "strings"
+import (
+	"database/sql"
+	"strings"
+)
 
 type FTS interface {
 	Index(contentID, content string) error
 	Match(term string) (contentIDs []string, err error)
+	Delete(contentID string) error
+	DeleteAll() error
 }
 
 func parseContentID(contentID string) (sitePrefix, resource string) {
@@ -23,13 +28,33 @@ func parseContentID(contentID string) (sitePrefix, resource string) {
 }
 
 type BlugeFTS struct {
+	LocalDir string
+}
+
+func (blugeFTS *BlugeFTS) Index(contentID, content string) error {
+	return nil
+}
+
+func (blugFTS *BlugeFTS) Match(term string) (contentIDs []string, err error) {
+	return contentIDs, nil
 }
 
 type SQLiteFTS struct {
+	DB *sql.DB
+}
+
+func (sqliteFTS *SQLiteFTS) Index(contentID, content string) error {
+	return nil
+}
+
+func (sqliteFTS *SQLiteFTS) Match(term string) (contentIDs []string, err error) {
+	return contentIDs, nil
 }
 
 type PostgresFTS struct {
+	DB *sql.DB
 }
 
 type MySQLFTS struct {
+	DB *sql.DB
 }
